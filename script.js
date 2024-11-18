80feb5a6f835536e0282ff91021a7aef69cd9c13
irodakarimov <irodakarimova896@gmail.com>
2024-11-18 18:51:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let space = {}
+
+space.name = prompt('ismingizni kiriting');
+space.id = prompt('space idingizni kiriting');
+space.password = prompt('space passwordingizni kiriting');
+
+console.log(space);
+
